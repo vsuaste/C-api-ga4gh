f243bf9846404185525b510a1d478dcaa9b3d3aa
Veronica <suaste@cimat.mx>
2015-06-30 10:30:15 -0400

    adding structures from schemas

---
+++ structures_schemas.h
@@ -0,0 +1,32 @@
+/*
+ These structures supose to created
+ from schemas.
+*/
+
+typedef struct {
+	char** alternateBases;
+	//wrong type
+	int calls;
+	int created;
+	long end;
+	char * id;
+	//wrong type
+	int info;
+	char** names;
+	char* referenceBases;
+	char* referenceName;
+	long start;
+	int updated;
+	char * variantSetId;
+} Variant;
+
+typedef struct {
+    char* name;
+	char* referenceName;
+	long int start;
+	long int end;
+	int pageSize;
+	char* pageToken;
+	char** callSetIds;
+	char** variantSetIds;
+}search_variant_request;
